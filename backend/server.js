01d61302d5f47d2c4607cc1a8cd43ad9d5477747
CdeCevin <kolivaresfuentes@gmail.com>
2025-09-14 20:30:11 -0300

    .

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,15 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // Aplica CORS antes de las rutas
 
 console.log('JWT_SECRET en server.js:', process.env.JWT_SECRET);
 
-
 const usuariosRoutes = require('./routes/usuariosRoute');
-const authRoutes = require('./routes/login');  // Ruta para login
+const authRoutes = require('./routes/login');
 const dueñosRoutes = require('./routes/dueñosRoute');
-
-const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors()); // Ahora sí, 'app' existe
 
 const PORT = 3001;
 
@@ -22,15 +20,14 @@ app.use('/api', authRoutes);
 
 // Prefijo base con id de clínica dinámico para rutas protegidas
 app.use('/api/:idClinica', (req, res, next) => {
-  req.clinicaId = req.params.idClinica;  // Guardas el id de clínica para usarlo en los controladores
+  req.clinicaId = req.params.idClinica;
   next();
 }, usuariosRoutes);
 
 app.use('/api/:idClinica', (req, res, next) => {
-  req.clinicaId = req.params.idClinica;  // Guardas el id de clínica para usarlo en los controladores
+  req.clinicaId = req.params.idClinica;
   next();
 }, dueñosRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
