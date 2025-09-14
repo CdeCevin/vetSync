5dc18ba1087b32554d957f9ad64d896fa0eccf9e
CdeCevin <kolivaresfuentes@gmail.com>
2025-09-13 21:34:10 -0300

    .

--- backend/controllers/verDB.js
+++ backend/controllers/verDB.js
@@ -1,7 +1,7 @@
 const connection = require('../db/connection');
 
 const verClinicas = (req, res) => {
-  connection.query('SELECT * FROM Clinicas', (error, results) => {
+  connection.query('SELECT * FROM Usuarios WHERE id_clinica = 1', (error, results) => {
     if (error) {
       console.error('Error al obtener las clínicas:', error);
       return res.status(500).json({ error: 'Error al obtener las clínicas' });
